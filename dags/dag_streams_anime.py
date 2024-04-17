53cdbfc25b92e06fe9920d19b7b4af37fbe32fb8
ackermanjayjay <reza23198@gmail.com>
2024-04-17 14:35:04 +0700

    build:etl extract from jikan and transform to add most fav anime and load to continue

--- dags/dag_streams_anime.py
+++ dags/dag_streams_anime.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import pandas as pd
 
 from datetime import datetime, timedelta
 
@@ -50,13 +50,27 @@ def stream_data():
     res = formattingData(res)
     return json.dumps(res, indent=3)
 
+def to_pandas():
+    res = getData()
+    res = formattingData(res)
+    df = pd.DataFrame(res)
+    return df
+    
+
+def most_anime_favorite():
+    data=to_pandas()
+    most_fav=data[["title","favorites"]].sort_values(by='favorites',ascending=False)
+    return most_fav.to_dict()
+
 
 with DAG(
     default_args=default_args,
-    dag_id="dag_with_api_data_anime_V01",
+    dag_id="dag_with_api_data_anime_V07",
     start_date=datetime(2024, 4, 10),
     schedule_interval="@once",
 ) as dag:
     task1 = PythonOperator(task_id="get_data", python_callable=stream_data)
+    task2 = PythonOperator(task_id='data_df', python_callable=to_pandas) 
+    task3 = PythonOperator(task_id='anime_fav', python_callable=most_anime_favorite) 
 
-    task1
+    task1 >> task2 >> task3
